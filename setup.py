506b9bd047dd96de5c11b5033f27f853d2c2b80a
hbeale <hbeale@gmail.com>
2024-10-08 13:50:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='mesa',  # Required
+    name='splicedice',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -125,7 +125,7 @@ setup(
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={'': 'mesa'},  # Optional
+    # package_dir={'': 'splicedice'},  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
@@ -136,7 +136,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=["mesa"],
+    packages=["splicedice"],
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
@@ -199,7 +199,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'mesa = mesa.__main__:main',
+            'splicedice = splicedice.__main__:main',
         ],
     },
 
