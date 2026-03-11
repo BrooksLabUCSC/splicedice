72e98d181f9a5be78b85bc7f0dd5bf2c1201e702
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2026-03-10 22:39:54 -0700

    build: update requirements.txt and set in setup.py
    -Pin tools to newer version. Old versions were not used anyways so there is no way to really know if this is safe.
    - Reference requirements.txt in the setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,14 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+def read_requirements(filename="requirements.txt"):
+    with open(path.join(here, filename), encoding="utf-8") as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.lstrip().startswith("#")
+        ]
+
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
@@ -151,13 +159,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-            "numpy",
-            "scipy",
-            "statsmodels",
-            "scikit-learn",
-            "umap-learn",
-            ],  # Optional
+    install_requires=read_requirements("requirements.txt"),
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
